2448a53cd4e7feaf929e6b7ffe8e5f0176ffe137
Shemar Dacosta <dshemar6@gmail.com>
2019-02-25 15:14:16 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,5 +2,25 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
-  
+
+  def new
+
+    @post = Post.new
+
+  end
+
+  def create
+    @post = Post.create(post_params)
+    redirect_to posts_path
+
+  end
+
+
+  private
+  # In controller actions, use this private method to access sea params from forms.
+  # Example: @sea.update(sea_params)
+  # check the return value of sea_params to see what you're working with!
+  def post_params
+    params.require(:post).permit(:title, :description)
+  end
 end
